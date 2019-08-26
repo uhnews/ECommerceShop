a4b592f6d3a091799b28770bbb0bba675a560f36
uhnews <54126262+uhnews@users.noreply.github.com>
2019-08-25 20:44:36 -0700

    Started Unit Tests

---
+++ ECommerceShop.WebUI.Tests/Controllers/BasketControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ECommerceShop.WebUI.Tests.Controllers
+{
+    [TestClass]
+    public class BasketControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- ECommerceShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ ECommerceShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -6,23 +6,40 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ECommerceShop.WebUI;
 using ECommerceShop.WebUI.Controllers;
+using ECommerceShop.Core.Contracts;
+using ECommerceShop.Core.Models;
+using ECommerceShop.Core.ViewModels;
 
 namespace ECommerceShop.WebUI.Tests.Controllers
 {
     [TestClass]
     public class HomeControllerTest
     {
+        //[TestMethod]
+        //public void Index()
+        //{
+        //    //// Arrange
+        //    //HomeController controller = new HomeController();
+
+        //    //// Act
+        //    //ViewResult result = controller.Index() as ViewResult;
+
+        //    //// Assert
+        //    //Assert.IsNotNull(result);
+        //}
+
         [TestMethod]
-        public void Index()
+        public void IndexPageDoesReturnProducts()
         {
-            //// Arrange
-            //HomeController controller = new HomeController();
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCategoryContext = new Mocks.MockContext<ProductCategory>();
 
-            //// Act
-            //ViewResult result = controller.Index() as ViewResult;
+            productContext.Insert(new Product());
+            HomeController controller = new HomeController(productContext, productCategoryContext);
+            var result = controller.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
 
-            //// Assert
-            //Assert.IsNotNull(result);
+            Assert.AreEqual(1, viewModel.Products.Count());
         }
     }
 }
